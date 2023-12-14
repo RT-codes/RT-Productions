423960c63b6ef26144c46719447455a334893eba
RT-codes <rowan.nicolay@gmail.com>
2023-12-14 16:03:27 +0100

    Changed some responsiveness issues - zoom/header/footer text

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.8"  />
+        <meta name="viewport" content="width=device-width, initial-scale=0.8 scale=1.0, user-scalable=no"  />
       </head>
       <body className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:p-36 sm:pb-0`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
