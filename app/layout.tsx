6a6d728d8b927e42dc537e93681036d9fc04202c
RT-codes <rowan.nicolay@gmail.com>
2023-12-15 11:49:56 +0100

    zooming issue seems solved by setting 'main' element to overflow hidden and removing padding by overriding them in global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.6, maximum-scale=1"></meta>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimim-scale=0.6, maximum-scale=1"></meta>
       </head>
-      <body className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:p-36 sm:pb-0 m-0 w-[100%] flex flex-col justify-center`}>
+      <body className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:p-36 sm:pb-0 m-0 w-[100%] flex flex-col justify-center p-0`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]"></div>
         <ActiveSectionContextProvider>
--- components/footer.tsx
+++ components/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
     const emailLink = `mailto:${email}?subject=MailFromRTProductions`;
     const tagline = contactData[0].tagline;
     return (
-        <div id='contact' ref={ref} className='flex flex-col pt-16 pb-2 justify-center items-center gap-2 relative bottom-0 h-[15rem] w-[100vw] bg-gray-200 bg-opacity-50 border-t-black/[0.03] border-2 mt-8 scroll-mt-28 mb-0'>
+        <div id='contact' ref={ref} className='flex flex-col pt-16 pb-2 justify-center items-center gap-2 relative bottom-0 h-[15rem] w-[100%] m-0 bg-gray-200 bg-opacity-50 border-t-black/[0.03] border-2 mt-8 scroll-mt-28 mb-0'>
             <div className='flex justify-center items-center gap-2'>
                 <a href={ emailLink } className='flex justify-center items-center gap-2 text-gray-400 hover:text-gray-600 '>
                     <div className='flex justify-center items-center  text-2xl bg-white w-[4rem] h-[4rem] rounded-[100%] border-4 box-border'>
